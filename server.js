6368806ddf33711b49dbbe6f1c7c4e490ff66d0c
Prince Mathew <i.princemathew@gmail.com>
2018-04-09 09:36:24 +0530

    DB test

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 const pg = require('pg');
+
 pg.connect('postgres://postgres:toor@localhost:5432/practicedocker');
+
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-server.get('/', (req, res) => res.status(200).send('hello'));
+
+server.get('/', (req, res) => {
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            res.send('error fetching client from pool ' + err)
+        }
+        client.query('SELECT $1::varchar AS my_first_query', ['names'], function (err, result) {
+            done()
+    
+            if (err) {
+             return res.send('error happened during query '+err)
+            }
+            res.json(result.rows[0])
+            process.exit(0)
+        })
+    });
+});
